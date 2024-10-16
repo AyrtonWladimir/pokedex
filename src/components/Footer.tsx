72d80fdeee48496d5c77cec47fb5354b3723388a
Ayrton Cabezas <ayrton.cabezas@seteinfo.com>
2024-10-16 15:32:51 -0500

    Interfaz y consumo de API

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import styles from "./footer.module.css";
+
+import PokemonPic from '../assets/pikachu.png';
+import LocationPic from '../assets/pointer.png';
+import ItemsPic from '../assets/pokeball.png';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Link className={styles.footerLink} to="/pokemons">
+        <img className={styles.footerIcon} src={PokemonPic} alt="Pokemon" />
+        Pokemons
+      </Link>
+      <Link
+        className={styles.footerLink}
+        onClick={(event) => event.preventDefault()}
+        to="/items"
+      >
+        <img className={styles.footerIcon} src={ItemsPic} alt="Items" />
+        Items
+      </Link>
+      <Link
+        className={styles.footerLink}
+        onClick={(event) => event.preventDefault()}
+        to="/location"
+      >
+        <img className={styles.footerIcon} src={LocationPic} alt="Mapa" />
+        Mapa
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,15 +1,48 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+import styles from "../pages/pokemons.module.css";
+import { fetchPokemons } from "../api/fetchPokemons";
+import { Pokemon } from "../types/types";
 
 const Pokemons = () => {
-  const [query,setQuery] = useState("BULBASAUR");
+  const [query, setQuery] = useState("");
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchAllPokemons = async () => {
+      const allPokemos = await fetchPokemons();
+      setPokemons(allPokemos);
+    };
+    fetchAllPokemons();
+  }, []);
+
   return (
     <>
       <Header query={query} setQuery={setQuery}></Header>
       <main>
-        <h1>POKEMONS</h1>
+        <nav className={styles.nav}>
+        {pokemons?.slice(0, 151).map((pokemon) => (
+            <Link
+              key={pokemon.id}
+              className={styles.listItem}
+              to={`/pokemons/${pokemon.name.toLowerCase()}`}
+            >
+              <img
+                className={styles.listItemIcon}
+                src={pokemon.imgSrc}
+                alt={pokemon.name}
+              />
+              <div className={styles.listItemText}>
+                <span>{pokemon.name}</span>
+                <span>{pokemon.id}</span>
+              </div>
+            </Link>
+          ))}
+        </nav>
       </main>
-      <footer>FOOTER</footer>
+      <Footer></Footer>
     </>
   );
 };
